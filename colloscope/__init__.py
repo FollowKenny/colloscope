9e8b91015bf47a97fcefadd4460585f64ff100b4
Ivan <iv.panico@gmail.com>
2024-10-28 19:18:00 +0100

    adding flake8, gitignore and some tests

---
+++ colloscope/__init__.py
@@ -0,0 +1,1 @@
+"""Gathers every functionnality to build a colloscope from several csv files."""
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from ortools.math_opt.python import mathopt
+"""Simple script to build the colloscope."""
+
+from colloscope.io import get_data
 
 
 def main():
-    # Build the model.
-    model = mathopt.Model(name="getting_started_lp")
-    x = model.add_variable(lb=-1.0, ub=1.5, name="x")
-    y = model.add_variable(lb=0.0, ub=1.0, name="y")
-    model.add_linear_constraint(x + y <= 1.5)
-    model.maximize(x + 2 * y)
+    """Build the colloscope."""
+    calendar, calendar_exclusions, students, teachers = get_data(
+        "C:/Users/ivpan/Documents/Dev/colloscope/tests"
+    )
 
 
 if __name__ == "__main__":
